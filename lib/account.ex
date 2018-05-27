51ca6c3be74f221254bbe1ed6338cfc2a9d81b9a
Andrew Forward <aforward@gmail.com>
2018-05-27 11:37:08 -0400

    Add Account upsert

--- lib/account.ex
+++ lib/account.ex
@@ -3,6 +3,13 @@ defmodule AcmeEx.Account do
 
   def new(client_key), do: create() |> insert(client_key)
 
+  def upsert(client_key) do
+    case fetch(client_key) do
+      {:ok, account} -> account
+      {:error, _reason} -> new(client_key)
+    end
+  end
+
   def fetch(client_key), do: AcmeEx.Db.fetch({:account, client_key})
 
   defp create() do
--- test/account_test.exs
+++ test/account_test.exs
@@ -1,13 +1,23 @@
 defmodule AcmeEx.AccountTest do
   use ExUnit.Case, async: true
 
-  test "new" do
+  alias AcmeEx.Account
+
+  test "new OK" do
     id = AcmeEx.Nonce.next()
-    assert %{id: id, contact: [], status: :valid} == AcmeEx.Account.new("abc123")
-    assert {:ok, %{id: id, contact: [], status: :valid}} == AcmeEx.Account.fetch("abc123")
+    assert %{id: id, contact: [], status: :valid} == Account.new("AcmeEx.AccountTest.abc123")
+
+    assert {:ok, %{id: id, contact: [], status: :valid}} ==
+             Account.fetch("AcmeEx.AccountTest.abc123")
+  end
+
+  test "upsert" do
+    id = AcmeEx.Nonce.next()
+    assert %{id: id, contact: [], status: :valid} == Account.upsert("AcmeEx.AccountTest.abc124")
+    assert %{id: id, contact: [], status: :valid} == Account.upsert("AcmeEx.AccountTest.abc124")
   end
 
   test "client_key" do
-    assert "jwk123" == AcmeEx.Account.client_key(%{protected: %{"jwk" => "jwk123"}})
+    assert "jwk123" == Account.client_key(%{protected: %{"jwk" => "jwk123"}})
   end
 end
